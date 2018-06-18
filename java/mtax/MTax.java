45d87e953bd64f2bdf4fc47a1ab800d00819a761
Amr Nady <amrnady17@gmail.com>
2018-06-18 02:58:45 +0200

    - refactoring validate method to make it more readable,optimized and legible

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -2,79 +2,113 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MTax implements Constant {
-    
-    public MTax(){
-        
-    }
-    
-    public static List<String> validate(List<X_Tax> xTaxList) {
-        
-        List<String> errorList = new ArrayList<>();
-        
-//        List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
-        
-        if(xTaxList != null && xTaxList.size() > 0) {
-            List<String> validIds = new ArrayList<>();
-            int cont = 0;
-            for (X_Tax tax : xTaxList) {
-                if(tax.getId() != null){
-                    validIds.add(tax.getId().toString());
-                }
-//                if(tax.getAmount() == null) {
-//                    errorList.add("El importe es obligatorio");
-//                }
+public class MTax implements Constant
+{
 
-                if(tax.getTax() == null) {
-                    errorList.add("El impuesto es obligatorio");
-                }
-//                else if(!taxCategoryList.contains(tax.getTax())) {
-//                    errorList.add("El impuesto no es un dato valido");
-//                }
+    public MTax()
+    {
 
-//                if(tax.isLocal()){
-//                    if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                } 
-//                else {
-//                    if(tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                }
-                
-                if(!tax.isLocal()){
-                    cont++;
-                }
-            }
-            if(cont<=0){
-                errorList.add("Debe de incluir al menos una tasa no local");
-            }
-            if(validIds.size() > 0){
-                    
-                    List<X_Tax> xt = TaxsByListId(validIds, false);
-                    if(xt.size() != validIds.size()){
-                        errorList.add("Existen datos no guardados previamente");
-                    }else{
-                        HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
-                        for(X_Tax tax: xt){
-                            map_taxs.put(tax.getId().toString(), tax);
-                        }
-                        for(int i = 0; i < xTaxList.size(); i++){
-                            if(xTaxList.get(i).getId() != null){
-                                xTaxList.get(i).setCreated(
-                                        map_taxs.get(xTaxList.get(i).getId().toString())
-                                                .getCreated());
-                            }
-                        }
-                    }
-            }
-        }
-//        else {
-//            errorList.add("El documento no tiene tasas");
-//        }
-        
-        return errorList;
     }
-    
+
+    public static List<String> validate(List<X_Tax> xTaxList)
+    {
+
+	List<String> errorList = new ArrayList<>();
+
+	// xTaxList.isEmpty() is better than xTaxList.size() > 0 in performance
+	// especially when using linkedlist
+	if (xTaxList != null && xTaxList.isEmpty())
+	{
+	    List<String> validIds = new ArrayList<>();
+
+	    int count = 0; // it's better to name variables with correct names
+			   // so use count not cont
+
+	    for (X_Tax tax : xTaxList)
+	    {
+		if (tax.getId() != null)
+		{
+		    validIds.add(tax.getId().toString());
+		}
+
+		if (tax.getTax() == null)
+		{
+		    errorList.add("El impuesto es obligatorio");
+		}
+
+		if (!tax.isLocal())
+		{
+		    count++;
+		}
+	    }
+
+	    if (count == 0) // you shouldn't use <=, you should use == as the value of count will not decrement
+	    {
+		errorList.add("Debe de incluir al menos una tasa no local");
+	    }
+
+	    // validIds.isEmpty() is better than validIds.size() > 0 in performance
+	    // especially when using linkedlist
+
+	    if (validIds.isEmpty())
+	    {
+
+		List<X_Tax> xTax = TaxsByListId(validIds, false); // I prefer using camel case in naming convention.
+		if (xTax.size() != validIds.size())
+		{
+		    errorList.add("Existen datos no guardados previamente");
+		} else
+		{
+		    HashMap<String, X_Tax> mapTaxs = new HashMap<String, X_Tax>(); // I prefer using camel case in naming convention.
+
+		    for (X_Tax tax : xTax)
+		    {
+			mapTaxs.put(tax.getId().toString(), tax);
+		    }
+
+		    for (int i = 0; i < xTaxList.size(); i++)
+		    {
+			if (xTaxList.get(i).getId() != null)
+			{
+			    xTaxList.get(i).setCreated(mapTaxs.get(xTaxList.get(i).getId().toString()).getCreated());
+			}
+		    }
+
+		}
+	    }
+	}
+
+	return errorList;
+
+	// else {
+	// errorList.add("El documento no tiene tasas");
+	// }
+
+	// It's better to make your playground branch and make comments at the end of
+	// your method
+	// when you merge to the master, you could remove them if you don't need them
+	// anymore.
+
+	// List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
+
+	// if(tax.getAmount() == null) {
+	// errorList.add("El importe es obligatorio");
+	// }
+
+	// else if(!taxCategoryList.contains(tax.getTax())) {
+	// errorList.add("El impuesto no es un dato valido");
+	// }
+
+	// if(tax.isLocal()){
+	// if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
+	// errorList.add("El importe es obligatorio");
+	// }
+	// }
+	// else {
+	// if(tax.getTaxAmount() == null ) {
+	// errorList.add("El importe es obligatorio");
+	// }
+	// }
+    }
+
 }
--- java/mtax/X_Tax.java
+++ java/mtax/X_Tax.java
@@ -3,8 +3,9 @@ import java.util.Date;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
-public class X_Tax {
-    
+public class X_Tax
+{
+
     private Object id;
     private BigDecimal base;
     private String tax;
@@ -20,119 +21,148 @@ public class X_Tax {
     private Date updated;
     private String version;
 
-    public X_Tax() {
+    public X_Tax()
+    {
     }
 
-    public Object getId() {
-        return id;
+    public Object getId()
+    {
+	return id;
     }
 
-    public void setId(Object id) {
-        this.id = id;
+    public void setId(Object id)
+    {
+	this.id = id;
     }
 
-    public BigDecimal getBase() {
-        return base;
+    public BigDecimal getBase()
+    {
+	return base;
     }
 
-    public void setBase(BigDecimal base) {
-        this.base = base;
+    public void setBase(BigDecimal base)
+    {
+	this.base = base;
     }
 
-    public String getTax() {
-        return tax;
+    public String getTax()
+    {
+	return tax;
     }
 
-    public void setTax(String tax) {
-        this.tax = tax;
+    public void setTax(String tax)
+    {
+	this.tax = tax;
     }
 
-    public String getTypeFactor() {
-        return typeFactor;
+    public String getTypeFactor()
+    {
+	return typeFactor;
     }
 
-    public void setTypeFactor(String typeFactor) {
-        this.typeFactor = typeFactor;
+    public void setTypeFactor(String typeFactor)
+    {
+	this.typeFactor = typeFactor;
     }
 
-    public BigDecimal getTasaOCuota() {
-        return tasaOCuota;
+    public BigDecimal getTasaOCuota()
+    {
+	return tasaOCuota;
     }
 
-    public void setTasaOCuota(BigDecimal tasaOCuota) {
-        this.tasaOCuota = tasaOCuota;
+    public void setTasaOCuota(BigDecimal tasaOCuota)
+    {
+	this.tasaOCuota = tasaOCuota;
     }
 
-    public BigDecimal getAmount() {
-        return amount;
+    public BigDecimal getAmount()
+    {
+	return amount;
     }
 
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
+    public void setAmount(BigDecimal amount)
+    {
+	this.amount = amount;
     }
 
-    public boolean isTrasladado() {
-        return trasladado;
+    public boolean isTrasladado()
+    {
+	return trasladado;
     }
 
-    public void setTrasladado(boolean trasladado) {
-        this.trasladado = trasladado;
-    }
-    
-    public boolean isLocal() {
-        return local;
+    public void setTrasladado(boolean trasladado)
+    {
+	this.trasladado = trasladado;
     }
 
-    public void setLocal(boolean local) {
-        this.local = local;
+    public boolean isLocal()
+    {
+	return local;
     }
 
-    public boolean isActive() {
-        return active;
+    public void setLocal(boolean local)
+    {
+	this.local = local;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
+    public boolean isActive()
+    {
+	return active;
     }
 
-    public String getCreatedBy() {
-        return createdBy;
+    public void setActive(boolean active)
+    {
+	this.active = active;
     }
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
+    public String getCreatedBy()
+    {
+	return createdBy;
     }
 
-    public String getUpdatedBy() {
-        return updatedBy;
+    public void setCreatedBy(String createdBy)
+    {
+	this.createdBy = createdBy;
     }
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
+    public String getUpdatedBy()
+    {
+	return updatedBy;
     }
 
-    public Date getCreated() {
-        return created;
+    public void setUpdatedBy(String updatedBy)
+    {
+	this.updatedBy = updatedBy;
     }
 
-    public void setCreated(Date created) {
-        this.created = created;
+    public Date getCreated()
+    {
+	return created;
     }
 
-    public Date getUpdated() {
-        return updated;
+    public void setCreated(Date created)
+    {
+	this.created = created;
     }
 
-    public void setUpdated(Date updated) {
-        this.updated = updated;
+    public Date getUpdated()
+    {
+	return updated;
     }
 
-    public String getVersion() {
-        return version;
+    public void setUpdated(Date updated)
+    {
+	this.updated = updated;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
+    public String getVersion()
+    {
+	return version;
     }
-    
+
+    public void setVersion(String version)
+    {
+	this.version = version;
+    }
+
 }
